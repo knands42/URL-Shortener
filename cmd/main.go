8d4fb3a4d64b620bcfd09c64e809234873c46812
Caio Fernandes <caiow.wk2@proton.me>
2024-09-30 23:37:46 -0300

    feat: add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"knands42/url-shortener/internal/database"
 	handler "knands42/url-shortener/internal/handlers"
 	"knands42/url-shortener/internal/server"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -25,5 +32,35 @@ func main() {
 
 	// Initialize the server
 	server.NewServer(r, handlers)
-	http.ListenAndServe(":3333", r)
+	gracefulShutdown()
+
+	err = http.ListenAndServe(":3333", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func gracefulShutdown() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan,
+		syscall.SIGHUP,  // kill -SIGHUP XXXX
+		syscall.SIGINT,  // kill -SIGINT XXXX or Ctrl+C
+		syscall.SIGQUIT, // kill -SIGQUIT XXXX
+	)
+	go func() {
+		// Wait for a signal
+		<-signalChan
+
+		fmt.Println("Shutting down the server...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		defer cancel()
+
+		select {
+		case <-time.After(30 * time.Second):
+			// TODO close connection
+		case <-ctx.Done():
+			fmt.Println("Graceful shutdown")
+		}
+	}()
 }
